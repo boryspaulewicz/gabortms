9f6893be862bc21f8aeed383c661848880cce6c7
Debian Live user <borys.paulewicz@gmail.com>
2016-04-25 21:23:58 +0000

    wskaznik losuje sie bardziej w srodku

--- gabortms.R
+++ gabortms.R
@@ -117,7 +117,7 @@ trial.code = function(trial, side = 'left', contrast = .5, duration = 64, withsc
         }, 'show-fixation' = {
             WINDOW$clear(c(.5, .5, .5))
             ## Losowa pozycja myszki
-            mouse.set.position(c(runif(1), .5) * WINDOW$get.size())
+            mouse.set.position(c(runif(1, min = .2, max = .8), .5) * WINDOW$get.size())
             ## Punkt fiksacji
             lapply(FX, WINDOW$draw)
             WINDOW$set.mouse.cursor.visible(F)
